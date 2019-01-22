a79c7d04ece81459a7caea7214db74823c04193c
Renaud de Villemeur <renaud.devillemeur@laposte.net>
2019-01-22 17:37:13 -0500

    Update Unit test to reflect change in points

--- clock-test.js
+++ clock-test.js
@@ -7,18 +7,18 @@ QUnit.module( "Clock Date", () => {
         t.equal( ClockDate.initializePositions(['A','B','c'])
                           .map((item) => item.point)
                           .toString()
-        , '{x:0, y:0},{x:0, y:0},{x:0, y:0}'
+        , '{x:0.00, y:0.00},{x:0.00, y:0.00},{x:0.00, y:0.00}'
         , "initializePositions passed")
   });
   test( "calculate new position", t => {
         t.deepEqual(ClockDate.getNewPosition(
             ClockDate.initializePositions(['A','B','c']),
-                                         Point.create(10, 10),
+                                         Point.create(10.0, 10.0),
                                          0.6,  
                                          [])
                      .map((item) => item.point)
                      .toString()
-        , '{x:-6, y:-6},{x:4, y:4},{x:-2, y:-2}'
+        , '{x:10.00, y:10.00},{x:-6.00, y:-6.00},{x:3.60, y:3.60}'
         , "get new position date passed");
 
       t.equal(Math.round(ClockDate.xOffset(10, 0, 0, Math.PI/3 ))
@@ -44,25 +44,25 @@ QUnit.module( "Clock Date", () => {
 
 QUnit.module( "Point", () => {
     test( "Point accessibility", t => {
-        t.equal(Point.create(10,10).toString(),
-        '{x:10, y:10}',
+        t.equal(Point.create(10.0,10.0).toString(),
+        '{x:10.00, y:10.00}',
         "initialize Point passed" );
     });
     test( "Point method", t => {
-        t.equal(Point.create(10,10).getDistance(Point.create(15, 15)).toString()
-        , '{x:5, y:5}'
+        t.equal(Point.create(10.0,10.0).getDistance(Point.create(15.0, 15.0)).toString()
+        , '{x:5.00, y:5.00}'
         , "get distance between point passed" );
         
-        t.equal(Point.create(10,10).addVector(Point.create(15, 15)).toString()
-        , '{x:25, y:25}'
+        t.equal(Point.create(10.0,10.0).addVector(Point.create(15.0, 15.0)).toString()
+        , '{x:25.00, y:25.00}'
         , "add vector to a point passed" );
 
-        t.equal(Point.create(10,10).multiply(0.6).toString()
-        , '{x:6, y:6}'
+        t.equal(Point.create(10.0,10.0).multiply(0.6).toString()
+        , '{x:6.00, y:6.00}'
         , "multiply point coordinate passed" );
 
         t.equal(Point.create(10.4,10.4).round().toString()
-        , '{x:10, y:10}'
+        , '{x:10.00, y:10.00}'
         , "add vector to a point passed" );
     });
 });
