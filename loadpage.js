15c0e0d77b9b2b8476354769abfde0ca7c0c24d6
Renaud de Villemeur <renaud.devillemeur@laposte.net>
2019-01-24 21:29:34 -0500

    Update clock creation parameter and remove async attribute, which caused some bug

--- loadpage.js
+++ loadpage.js
@@ -1,10 +1,7 @@
-function initClock()
-{
-    window.MyClock = Clock.create();
+function initClock() {
+    window.MyClock = Clock.create(40, 40, 0.04);
     common.addEvent(document.getElementById("start_clock"), 'click', () => { MyClock.startClock(); });
     common.addEvent(document.getElementById("stop_clock"), 'click', () => { MyClock.stopClock(); });
-    
     MyClock.startClock();
 }
-
 common.addEvent(window, 'load', initClock);
