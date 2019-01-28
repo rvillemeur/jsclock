2f67a3a181945ad93811a93663e5910c56604f43
Renaud de Villemeur <renaud.devillemeur@laposte.net>
2019-01-27 22:05:53 -0500

    install mocha and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,34 @@
+const fs = require('fs');
+
 module.exports = {
   mode: 'development',
   entry: './main.js',
   output: {
     filename: 'main.js',
     publicPath: 'dist'
+  },
+  devServer: {
+    https: true,
+    https: {
+      key: fs.readFileSync('C://devzone//ssl//server.key'),
+      cert: fs.readFileSync('C://devzone//ssl//server.crt'),
+      ca: fs.readFileSync('C://devzone//ssl//server.pem')
+    },
+    index: 'Clock.html',
+    port: 8081
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
