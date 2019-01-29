59d17106198660b4e49865889dea2fd48eccf35f
Renaud de Villemeur <renaud.devillemeur@laposte.net>
2019-01-29 14:48:03 -0500

    refactoring

--- test/point.test.js
+++ test/point.test.js
@@ -2,32 +2,39 @@ import Point from '../src/point.js'
 import assert from 'assert'
 
 describe('Point accessibility', function () {
-  describe('#Point.create', function () {
-    it('should return a point', function () {
-      assert.strict.equal('{x:10.00, y:10.00}', Point.create(10.0, 10.0).toString())
-    })
+  it('should return a point', function () {
+    const point1 = Point.create(10.0, 10.0)
+    assert.strict.equal('{x:10.00, y:10.00}', point1.toString())
   })
 })
 
 describe('Point Method', function () {
-  describe('#Point.gestdistance', function () {
-    it('get distance between point', function () {
-      assert.strict.equal('{x:5.00, y:5.00}', Point.create(10.0, 10.0).getDistance(Point.create(15.0, 15.0)).toString())
-    })
+  it('get distance between two point', function () {
+    const point1 = Point.create(15.0, 15.0)
+    const point2 = Point.create(10.0, 10.0)
+    const distance = point2.getDistance(point1)
+    assert.strict.equal('{x:5.00, y:5.00}', distance.toString())
   })
-  describe('#Point.addVector', function () {
-    it('add vector to a point', function () {
-      assert.strict.equal('{x:25.00, y:25.00}', Point.create(10.0, 10.0).addVector(Point.create(15.0, 15.0)).toString())
-    })
+})
+describe('#Point.addVector', function () {
+  it('add vector to a point', function () {
+    const point1 = Point.create(15.0, 15.0)
+    const point2 = Point.create(10.0, 10.0)
+    const vector = point2.addVector(point1)
+    assert.strict.equal('{x:25.00, y:25.00}', vector.toString())
   })
-  describe('#Point.multiply', function () {
-    it('multiply point coordinate', function () {
-      assert.strict.equal('{x:6.00, y:6.00}', Point.create(10.0, 10.0).multiply(0.6).toString())
-    })
+})
+describe('#Point.multiply', function () {
+  it('multiply point coordinate', function () {
+    const point1 = Point.create(10.0, 10.0)
+    const point2 = point1.multiply(0.6)
+    assert.strict.equal('{x:6.00, y:6.00}', point2.toString())
   })
-  describe('#Point.round', function () {
-    it('round point coordinate', function () {
-      assert.strict.equal('{x:10.00, y:10.00}', Point.create(10.4, 10.4).round().toString())
-    })
+})
+describe('#Point.round', function () {
+  it('round point coordinate', function () {
+    const point1 = Point.create(10.4, 10.4)
+    const point2 = point1.round()
+    assert.strict.equal('{x:10.00, y:10.00}', point2.toString())
   })
 })
