c413f16cb2ec3e166411007ba9828e67a2862b75
Renaud de Villemeur <renaud.devillemeur@laposte.net>
2019-04-25 14:37:42 -0400

    move clock surround to MVP Pattern (kind of)

---
+++ src/ClockSurround.js
@@ -0,0 +1,201 @@
+import Point from './point.js'
+
+const ClockSurroundModel = Object.assign(Object.create(Object.prototype), {
+  initializePositions: function (labelArray) {
+    return labelArray.map((label) => Point.create(0, 0))
+  },
+  // this signature is needed because it's a recursive function.
+  getNewPosition: function getNewPosition (OriginalpositionArray, initPoint, speed, newPositionArray) {
+    if (OriginalpositionArray.length === 0) {
+      return newPositionArray
+    }
+    const currentPoint = OriginalpositionArray.shift()
+    newPositionArray.push(initPoint)
+
+    const newPoint = currentPoint.addVector(
+      initPoint.getDistance(currentPoint).multiply(speed))
+
+    return getNewPosition(OriginalpositionArray, newPoint, speed, newPositionArray)
+  },
+  update: function (point) {
+    const newPositionArray = this.getNewPosition(this.positionArray(), point, this.speed(), [])
+    this.positionArray(newPositionArray)
+    this.updateCurrStep()
+  },
+  getDisplayPosition: function (positionArray) {
+    const pos = positionArray.map((position, index) => {
+      return Point.create(this.x(position, index), this.y(position, index))
+    })
+    return pos
+  },
+  xOffset: function (width, angle) {
+    return width * Math.cos(angle)
+  },
+  yOffset: function (heigth, angle) {
+    return heigth * Math.sin(angle)
+  },
+  angle: function (currStep, index, split) {
+    return currStep + index * split
+  },
+  x: function (position, index) {
+    return Math.round(position.x) +
+            this.xOffset(this.clockWidth(), this.angle(this.currStep(), index, this.circleSplit()))
+  },
+  y: function (position, index) {
+    return Math.round(position.y) +
+            this.yOffset(this.clockHeight(), this.angle(this.currStep(), index, this.circleSplit()))
+  },
+  updateCurrStep: function () {
+    // dummy fonction, so that update is common to clock and surrond.
+  },
+  initialize: function (clockWidth, clockHeight, speed) {
+    this.clockWidth(clockWidth)
+    this.clockHeight(clockHeight)
+    this.speed(speed)
+
+    return this
+  },
+  create: function () {
+    var self = Object.create(this)
+
+    const _labelArray = '1 2 3 4 5 6 7 8 9 10 11 12'.split(' ')
+    self.labelArray = () => { return _labelArray }
+
+    const _circleSplit = 2 * Math.PI / _labelArray.length
+    self.circleSplit = () => { return _circleSplit }
+
+    var _clockWidth = 0
+    self.clockWidth = function (clockWidth) {
+      if (arguments.length > 0) {
+        _clockWidth = clockWidth
+      } else {
+        return _clockWidth
+      }
+    }
+
+    var _clockHeight = 0
+    self.clockHeight = function (clockHeight) {
+      if (arguments.length > 0) {
+        _clockHeight = clockHeight
+      } else {
+        return _clockHeight
+      }
+    }
+
+    var _speed = 0
+    self.speed = function (speed) {
+      if (arguments.length > 0) {
+        _speed = speed
+      } else {
+        return _speed
+      }
+    }
+
+    var _currStep = 0
+    self.currStep = function (currStep) {
+      if (arguments.length > 0) {
+        _currStep = currStep
+      } else {
+        return _currStep
+      }
+    }
+
+    var _positionArray = this.initializePositions(_labelArray)
+    self.positionArray = function (positionArray) {
+      if (arguments.length > 0) {
+        _positionArray = positionArray
+      } else {
+        return _positionArray
+      }
+    }
+
+    return self
+  }
+})
+
+var ClockDateView = Object.assign(Object.create(Object.prototype), {
+  createAndAttachHtmlElement: function createAndAttachHtmlElement (display, tag, label) {
+    const element = display.document.createElement('div')
+
+    element.appendChild(display.document.createTextNode(label))
+    element.style.cssText = 'position:absolute'
+    element.classList.add('clock')
+    tag.appendChild(element)
+
+    return element
+  },
+  initializeHtmlElements: function (display, labelArray) {
+    const tag = display.document.getElementsByTagName('body')[0]
+    return labelArray.map(label => this.createAndAttachHtmlElement(display, tag, label))
+  },
+  updateCssPosition: function (htmlElement, x, y) {
+    htmlElement.style.left = x + 'px'
+    htmlElement.style.top = y + 'px'
+  },
+  updatePosition: function (positionList) {
+    this.elementList().forEach((element, index) => {
+      const position = positionList[index]
+      this.updateCssPosition(element, position.x, position.y)
+    })
+  },
+  initialize: function (display, labelArray) {
+    this.elementList(this.initializeHtmlElements(display, labelArray))
+
+    return this
+  },
+  create: function create (display, labelArray) {
+    var self = Object.create(this)
+
+    var _elementList = []
+    self.elementList = function (elementList) {
+      if (arguments.length > 0) {
+        _elementList = elementList
+      } else {
+        return _elementList
+      }
+    }
+
+    return self
+  }
+})
+
+// presenter object
+const ClockSurround = Object.assign({}, {
+  move: function (point) {
+    const model = this.model()
+    model.update(point)
+    this.view().updatePosition(model.getDisplayPosition(model.positionArray()))
+  },
+  initialize: function (display, clockWidth, clockHeight, speed) {
+    this.model(ClockSurroundModel.create().initialize(clockWidth, clockHeight, speed))
+
+    this.view(ClockDateView.create().initialize(display, this.model().labelArray()))
+
+    return this
+  },
+  create: function () {
+    var self = Object.create(this)
+
+    var _model = {}
+    self.model = function (model) {
+      if (arguments.length > 0) {
+        _model = model
+      } else {
+        return _model
+      }
+    }
+
+    var _view = {}
+    self.view = function (view) {
+      if (arguments.length > 0) {
+        _view = view
+      } else {
+        return _view
+      }
+    }
+
+    return self
+  }
+})
+
+export { ClockSurround as default }
--- src/clock.js
+++ src/clock.js
@@ -3,12 +3,13 @@ import addEvent from './common.js'
 import Point from './point.js'
 import ClockDate from './clockDate.js'
 import { ClockNeedleSecond, ClockNeedleHour, ClockNeedleMinute } from './clockNeedle.js'
+import ClockSurround from './ClockSurround.js'
 
 const Clock = Object.assign({}, {
   move: function () {
     const position = Point.create(MousePosition.x + 75, MousePosition.y + 75)
     this.date().move(position)
-    this.surround().update(position)
+    this.surround().move(position)
     this.needlesSecond().move(position)
     this.needlesMinute().move(position)
     this.needlesHour().move(position)
@@ -21,7 +22,6 @@ const Clock = Object.assign({}, {
   stopClock: function () {
     clearInterval(this.timer)
   },
-
   create: function create () {
     const self = Object.create(this)
 
@@ -43,7 +43,7 @@ const Clock = Object.assign({}, {
     const _date = ClockDate.create().initialize(window, _clockWidth * 1.5, _clockHeight * 1.5, _speed)
     self.date = () => { return _date }
 
-    const _surround = ClockSurround.create(_clockWidth, _clockHeight, _speed)
+    const _surround = ClockSurround.create().initialize(window, _clockWidth, _clockHeight, _speed)
     self.surround = () => { return _surround }
 
     const _needlesSecond = ClockNeedleSecond.create().initialize(window, _clockWidth / 4.5, _clockHeight / 4.5, _speed)
@@ -60,109 +60,4 @@ const Clock = Object.assign({}, {
   }
 })
 
-const ClockCommonTraits = {
-  createHtmlElement: function createHtmlElement (label) {
-    const element = window.document.createElement('div')
-
-    element.appendChild(window.document.createTextNode(label))
-    element.style.cssText = 'position:absolute'
-    element.classList.add('clock')
-
-    return element
-  },
-  updateCssPosition: function updateCssPosition (htmlElement, x, y) {
-    htmlElement.style.left = x + 'px'
-    htmlElement.style.top = y + 'px'
-  },
-  initializePositions: function initializePositions (labelArray) {
-    return labelArray.map(label => this.position(label))
-  },
-  position: function position (label) {
-    const htmlElement = this.createHtmlElement(label)
-    this.attachHtmlToBody(htmlElement)
-    return { point: Point.create(0, 0), html: htmlElement }
-  },
-  attachHtmlToBody: function attachHtmlToBody (htmlElement) {
-    const tagBody = window.document.getElementsByTagName('body')[0]
-    tagBody.appendChild(htmlElement)
-  },
-  getNewPosition: function getNewPosition (OriginalpositionList, initPoint, speed, newPositionList) {
-    if (OriginalpositionList.length === 0) {
-      return newPositionList
-    }
-    const currentPosition = OriginalpositionList.shift()
-    newPositionList.push({ point: initPoint, html: currentPosition.html })
-
-    const currentPoint = currentPosition.point
-    const newPoint = currentPoint.addVector(
-      initPoint.getDistance(currentPoint).multiply(speed))
-
-    return getNewPosition(OriginalpositionList, newPoint, speed, newPositionList)
-  },
-  xOffset: function xOffset (width, angle) {
-    return width * Math.cos(angle)
-  },
-  yOffset: function yOffset (heigth, angle) {
-    return heigth * Math.sin(angle)
-  },
-  angle: function angle (currStep, index, split) {
-    return currStep + index * split
-  },
-  x: function x (position, index) {
-    return Math.round(position.point.x) +
-            this.xOffset(this.clockWidth, this.angle(this.currStep, index, this.circleSplit))
-  },
-  y: function y (position, index) {
-    return Math.round(position.point.y) +
-            this.yOffset(this.clockHeight, this.angle(this.currStep, index, this.circleSplit))
-  },
-  update: function update (point) {
-    this.positionList = this.getNewPosition(this.positionList, point, this.speed, [])
-    this.updateCurrStep()
-    this.draw()
-  },
-  draw: function draw () {
-    this.positionList.forEach((position, index) => {
-      this.updateCssPosition(position.html, this.x(position, index), this.y(position, index))
-    })
-  }
-}
-
-const ClockSurround = Object.assign({}, ClockCommonTraits, {
-  updateCurrStep: function updateCurrStep () {
-    // dummy fonction, so that update is common to clock and surrond.
-
-  },
-
-  create: function create (clockWidth, clockHeight, speed) {
-    const self = Object.create(this)
-
-    const surroundArray = '1 2 3 4 5 6 7 8 9 10 11 12'.split(' ')
-    Object.defineProperty(self, 'clockWidth', {
-      value: clockWidth,
-      writable: false
-    })
-    Object.defineProperty(self, 'clockHeight', {
-      value: clockHeight,
-      writable: false
-    })
-    Object.defineProperty(self, 'speed', {
-      value: speed,
-      writable: false
-    })
-    Object.defineProperty(self, 'currStep', {
-      value: -60 * Math.PI / 180,
-      writable: false
-    })
-    // circle circumference = 2 * Math.PI * R
-    Object.defineProperty(self, 'circleSplit', {
-      value: 2 * Math.PI / surroundArray.length,
-      writable: false
-    })
-    self.positionList = this.initializePositions(surroundArray)
-
-    return self
-  }
-})
-
-export { Clock as default, ClockDate, ClockSurround, ClockCommonTraits }
+export { Clock as default, ClockDate }
